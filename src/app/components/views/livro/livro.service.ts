bc5778f401c27b4b6cfb28de439ce19e556d286b
patryckLisboa <patryck-lisboa@hotmail.com>
2021-03-04 15:07:02 -0300

    delete para livro implementado

--- src/app/components/views/livro/livro.service.ts
+++ src/app/components/views/livro/livro.service.ts
@@ -31,6 +31,10 @@ export class LivroService {
     const url = `${this.baseUrl}/livros?categoria=${id_cat}`;
     return this.http.post<Livro[]>(url, livro)
   }
+  delete(id: String) : Observable<void>{
+    const url = `${this.baseUrl}/livros/${id}`
+    return this.http.delete<void>(url)
+  }
 
   mensagem(str : String): void{
     this._snack.open(`${str}`, 'OK', {
